b9106b2480739b086441cd275970e2b791deff86
EivydasLeo <eivydasleo@gmail.com>
2025-12-14 14:49:35 +0200

    Refactpred api routes

--- src/api/ghibliApi.ts
+++ src/api/ghibliApi.ts
@@ -1,16 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { Film, People } from "../types/types";
+
+const BASE_URL = "https://ghibliapi.vercel.app/";
 
 export const ghibliApi = createApi({
     reducerPath: "ghibliApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "https://ghibliapi.vercel.app/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
     endpoints: (builder) => ({
-        getFilms: builder.query({
+        getFilms: builder.query<Film[], void>({
             query: () => "films",
         }),
-        getPeopleByFilmId: builder.query({
-            query: (filmId) => `films/${filmId}/people`,
+
+        getPeople: builder.query<People[], void>({
+            query: () => "people",
         }),
     }),
 });
 
-export const { useGetFilmsQuery, useGetPeopleByFilmIdQuery } = ghibliApi;
+export const { useGetFilmsQuery, useGetPeopleQuery } = ghibliApi;
