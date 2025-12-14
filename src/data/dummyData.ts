09fccdfadb1440cb5a803effffb42766f548c787
EivydasLeo <eivydasleo@gmail.com>
2025-12-14 14:50:11 +0200

    Added new interface and updated dummyData.

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -5,3 +5,28 @@ export const aboutContent: AboutContent = {
     paragraph:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa excepturi, porro molestiae quo, ea repellat molestias, sit exercitationem labore ratione sed perspiciatis? Non, autem sint. Non assumenda a sapiente dicta ullam adipisci reiciendis quisquam esse optio dolorem repudiandae tempora vero quibusdam quod labore beatae recusandae veniam, maiores, nam quis qui.",
 };
+
+export const filmsPageText = {
+    filmsLoadError: "There was an error loading films.",
+    peopleLoadError: "There was an error loading people.",
+    noPeopleFound: "No people found for this film.",
+};
+
+export const peopleTableText = {
+    defaultCaption: "People in Film Title:",
+    rowNumberAriaLabel: "Row number",
+    nameHeader: "Name",
+    ageHeader: "Age",
+    genderHeader: "Gender",
+    eyeColorHeader: "Eye colour",
+};
+
+export const layoutText = {
+    navigationHeading: "Navigation",
+    mainNavigationAriaLabel: "Main navigation",
+};
+
+export const layoutNavItems = [
+    { to: "/", label: "About", end: true },
+    { to: "/films", label: "Films" },
+];
--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,12 @@ export interface Film {
 export interface FilmCardProps extends Film {
     onShowPeople: () => void;
 }
+
+export interface People {
+    id?: string;
+    name: string;
+    age: string;
+    gender: string;
+    eye_color: string;
+    films?: string[];
+}
