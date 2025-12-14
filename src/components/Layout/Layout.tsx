ec0d227147e2acbd6c15836958e02c98609d5fc0
EivydasLeo <eivydasleo@gmail.com>
2025-12-14 14:53:40 +0200

    Layout file update.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
+import type { PropsWithChildren } from "react";
+import { NavLink } from "react-router-dom";
+import { layoutNavItems, layoutText } from "../../data/dummyData";
 import styles from "./Layout.module.css";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: PropsWithChildren) {
     return (
         <div className={styles.container}>
             <aside className={styles.sidebar}>
-                <h3 className={styles.heading}>Navigation</h3>
-                <nav aria-label="Main navigation">
+                <h3 className={styles.heading}>{layoutText.navigationHeading}</h3>
+                <nav aria-label={layoutText.mainNavigationAriaLabel}>
                     <ul className={styles.navList}>
-                        <li className={styles.navItem}>
-                            <Link className={styles.navLink} to="/">
-                                About
-                            </Link>
-                        </li>
-                        <li className={styles.navItem}>
-                            <Link className={styles.navLink} to="/films">
-                                Films
-                            </Link>
-                        </li>
+                        {layoutNavItems.map((item) => (
+                            <li key={item.to} className={styles.navItem}>
+                                <NavLink
+                                    to={item.to}
+                                    end={item.to === "/"}
+                                    className={({ isActive }) =>
+                                        `${styles.navLink} ${isActive ? styles.navLinkActive : ""}`
+                                    }
+                                >
+                                    {item.label}
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </aside>
